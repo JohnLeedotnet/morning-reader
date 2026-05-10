e5a1653f18d8a9a6ddd22915c1e1f44594019960
Jianhong Li <lijhm@protonmail.com>
2026-05-10 23:31:25 +0800

    Initial Commit

---
+++ Morning ReaderTests/Morning_ReaderTests.swift
@@ -0,0 +1,18 @@
+//
+//  Morning_ReaderTests.swift
+//  Morning ReaderTests
+//
+//  Created by Jianhong Li on 10/5/2026.
+//
+
+import Testing
+
+struct Morning_ReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
